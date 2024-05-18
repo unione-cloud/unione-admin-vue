c78c5c1f8c96f471b24b758313e995031fa2fea2
jeking <jeking217@163.com>
2024-05-18 21:48:02 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import Antd from 'ant-design-vue'
+import * as Icons from '@ant-design/icons-vue'
+import 'ant-design-vue/dist/reset.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(Antd)
+Object.keys(Icons).forEach((key: any) => {
+  app.component(key, Icons[key])
+})
+
+app.mount('#app')
