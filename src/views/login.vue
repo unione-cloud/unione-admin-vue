aeffe65d2e10fba621f52a07f457acf36a75378e
unione-cloud <unione-cloud@163.com>
2024-06-17 23:36:23 +0800

    完成登录页面布局

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,42 +1,100 @@
 <template>
-  <div class="unione-login">
-    <div class="login-form">
-      <h1 class="title">系统登录</h1>
-      <a-form :model="formData" :rules="formRules" autocomplete="off" ref="loginForm">
-        <a-form-item label="用户帐号" name="username">
-          <a-input v-model:value="formData.username" />
-        </a-form-item>
+  <div class="unione-login" :style="{ 'background-image': 'url(' + ImageBg + ')' }">
+    <div class="login-box">
+      <div class="box-head">
+        <div class="app-info">
+          <div class="app-title">值班系统</div>
+          <div class="sub-title">欢迎使用智能值班系统</div>
+        </div>
+        <div class="app-qr"><img :src="ImageQr" /></div>
+      </div>
+      <div class="box-body">
+        <div class="ads-box">
+          <img class="ad-pic" :src="ImageAd" />
+        </div>
+        <div class="login-form">
+          <a-form
+            :model="formData"
+            :rules="formRules"
+            autocomplete="off"
+            ref="loginForm"
+            :label-col="{ span: 7 }"
+          >
+            <a-tabs v-model:activeKey="loginType">
+              <a-tab-pane key="username" tab="帐号登录">
+                <a-form-item label="用户帐号" name="username">
+                  <a-input v-model:value="formData.username" />
+                </a-form-item>
 
-        <a-form-item label="用户密码" name="password">
-          <a-input-password v-model:value="formData.password" />
-        </a-form-item>
+                <a-form-item label="用户密码" name="password">
+                  <a-input-password v-model:value="formData.password" />
+                </a-form-item>
+              </a-tab-pane>
+              <a-tab-pane key="userphone" tab="手机登录" force-render>
+                <a-form-item label="手机号码" name="userphone">
+                  <a-input v-model:value="formData.userphone" />
+                </a-form-item>
 
-        <a-form-item>
-          <a-form-item name="remember" no-style>
-            <a-checkbox v-model:checked="formData.remember">记住我</a-checkbox>
-          </a-form-item>
-          <a class="forgot-pwd" href="#">忘记密码</a>
-        </a-form-item>
+                <a-form-item label="验证码" name="smscode">
+                  <a-input v-model:value="formData.smscode" />
+                </a-form-item>
+              </a-tab-pane>
+            </a-tabs>
 
-        <a-form-item>
-          <a-button type="primary" class="btn-login" @click="toLogin">提交</a-button>
-        </a-form-item>
-      </a-form>
+            <a-form-item>
+              <a-form-item name="remember" no-style>
+                <a-checkbox v-model:checked="formData.remember">记住我</a-checkbox>
+              </a-form-item>
+              <a class="forgot-pwd" href="#">忘记密码</a>
+            </a-form-item>
+
+            <a-form-item>
+              <a-button type="primary" class="btn-login" @click="toLogin">登录</a-button>
+            </a-form-item>
+            <a-form-item class="service-opts">
+              <a-checkbox
+                >我已阅读并同意<span class="link">服务协议</span>和<span class="link"
+                  >隐私政策</span
+                ></a-checkbox
+              >
+            </a-form-item>
+          </a-form>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import type { Rule } from 'ant-design-vue/es/form'
 import axios from '@/utils/axios'
 
+import ImageBg from '@/assets/login/bg.jpg'
+import ImageAd from '@/assets/login/ad1.png'
+import ImageQr from '@/assets/login/qr.png'
+
+const loginType = ref('username')
+watch(loginType, () => {
+  if (loginType.value == 'username') {
+    formRules.value = {
+      username: [{ required: true, message: '请输入用户帐号', trigger: 'change' }],
+      password: [{ required: true, message: '请输入用户密码', trigger: 'change' }]
+    }
+  } else {
+    formRules.value = {
+      userphone: [{ required: true, message: '请输入用户密码', trigger: 'change' }],
+      smscode: [{ required: true, message: '请输入用户密码', trigger: 'change' }]
+    }
+  }
+})
+
 const loginForm = ref()
 const formData = ref<any>({})
-const formRules: Record<string, Rule[]> = {
+const formRules = ref<Record<string, Rule[]>>({
   username: [{ required: true, message: '请输入用户帐号', trigger: 'change' }],
   password: [{ required: true, message: '请输入用户密码', trigger: 'change' }]
-}
+})
 
 // 登录表单提交方法
 const toLogin = () => {
@@ -52,26 +110,96 @@ const toLogin = () => {
 .unione-login {
   width: 100vw;
   height: 100vh;
+  background-repeat: repeat-x;
+  background-size: auto 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-  .login-form {
-    position: absolute;
-    top: 20%;
-    left: 30%;
-    width: 40%;
-    height: 350px;
-    border: 1px solid #eeeeee;
-    padding: 20px 40px;
+  .login-box {
+    width: 70%;
+    height: 60%;
+    background-color: #ffffff;
+    border-radius: 10px;
 
-    .title {
-      text-align: center;
+    .box-head {
+      height: 50px;
+      display: flex;
+      padding: 10px;
+
+      .app-info {
+        width: 150%;
+        .app-title {
+          color: #134ce4;
+          font-size: 30px;
+          font-weight: bold;
+          text-align: center;
+          user-select: none;
+        }
+        .sub-title {
+          font-size: 14px;
+          text-align: center;
+        }
+      }
+
+      .app-qr {
+        width: 100%;
+        img {
+          width: 60px;
+          height: 50px;
+          float: right;
+          cursor: pointer;
+        }
+      }
     }
 
-    .forgot-pwd {
-      float: right;
-    }
+    .box-body {
+      display: flex;
+      height: calc(100% - 50px);
 
-    .btn-login {
-      width: 100%;
+      .ads-box {
+        width: 55%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .ad-pic {
+          width: 90%;
+        }
+      }
+
+      .login-form {
+        width: 45%;
+        height: 100%;
+        padding: 20px 40px;
+        margin-top: 20px;
+
+        .title {
+          text-align: center;
+        }
+
+        .forgot-pwd {
+          float: right;
+        }
+
+        .btn-login {
+          width: 100%;
+        }
+
+        .service-opts {
+          font-size: 10px;
+          transform: scale(0.7);
+          transform-origin: top left;
+          margin-top: -15px;
+
+          .link {
+            margin: auto 3px;
+            color: #4096ff;
+            cursor: pointer;
+          }
+        }
+      }
     }
   }
 }
