030026a0a89089003336a7b1cb0e2d0d6330f85c
unione-cloud <unione-cloud@163.com>
2025-03-21 08:55:24 +0800

    角色分配

--- src/views/system/permis/user/list.vue
+++ src/views/system/permis/user/list.vue
@@ -5,12 +5,48 @@
 
     <a-drawer
       :title="drawer.title"
-      :width="550"
+      :width="900"
       v-model:visible="drawer.visible"
       :placement="drawer.placement"
       class="drawer-form"
     >
-      <unione-form :form="drawer.form" ref="form"></unione-form>
+      <div class="warp">
+        <a-row>
+          <a-col :span="16">
+            <unione-page-list
+              ref="rolelist"
+              v-bind="defineRoleList"
+              @btnClick="btnClick"
+            ></unione-page-list>
+          </a-col>
+          <a-col :span="8">
+            <a-list size="small" bordered :data-source="hadRoleList">
+              <template #renderItem="{ item }">
+                <a-list-item>{{ item }}</a-list-item>
+              </template>
+              <template #header>
+                <div>已有角色</div>
+              </template>
+            </a-list>
+            <a-list size="small" bordered :data-source="addRoleList" style="margin-top: 10px">
+              <template #renderItem="{ item }">
+                <a-list-item>{{ item }}</a-list-item>
+              </template>
+              <template #header>
+                <div>新增角色</div>
+              </template>
+            </a-list>
+            <a-list size="small" bordered :data-source="delRoleList" style="margin-top: 10px">
+              <template #renderItem="{ item }">
+                <a-list-item>{{ item }}</a-list-item>
+              </template>
+              <template #header>
+                <div>删除角色</div>
+              </template>
+            </a-list>
+          </a-col>
+        </a-row>
+      </div>
 
       <div class="btns">
         <a-button type="primary" @click="drawer.tosave">保存</a-button>
@@ -26,6 +62,8 @@ import { useRouter, type Router } from 'vue-router'
 
 const router: Router = useRouter()
 const page = ref()
+const rolelist = ref()
+
 const define = ref({
   storage: {
     controller: '/api/system/user'
@@ -116,14 +154,15 @@ const define = ref({
 
 function btnClick({ btn, event, row, keys }: any) {
   console.log('table btn click', btn, event, row)
-  if (btn.name == 'edit') {
+  if (btn.name == 'role-assign') {
     drawer.value.visible = true
-    drawer.value.title = '编辑用户'
+    drawer.value.title = '角色分配'
     drawer.value.placement = 'right'
     drawer.value.row = row
-    nextTick(() => {
-      form.value.setValue(row)
-    })
+  }
+  if (btn.name == 'assignBatch') {
+    const selected = rolelist.value.getSelected()
+    console.log('selected', selected)
   }
   if (btn.name == 'rolelist') {
     router.push({
@@ -135,125 +174,65 @@ function btnClick({ btn, event, row, keys }: any) {
   }
 }
 
-const form = ref() //form ref obj
+const defineRoleList = ref({
+  storage: {
+    controller: '/api/system/role'
+  },
+  fields: [
+    {
+      title: '类型',
+      name: 'types',
+      control: 'unione-select-box',
+      value: 0,
+      convert: {
+        types: 'dict',
+        dictName: 'ROLETYPE'
+      }
+    },
+    {
+      title: '名称',
+      name: 'name'
+    },
+    {
+      title: '编码',
+      name: 'sn'
+    }
+  ],
+  leftBtns: false,
+  rightBtns: false,
+  queryBtns: [
+    {
+      name: 'assignBatch',
+      title: '批量分配',
+      type: 'primary',
+      icon: 'PlusOutlined'
+    }
+  ],
+  operation: {
+    title: '操作',
+    width: 90,
+    btns: [
+      'view',
+      'edit',
+      'delete',
+      {
+        name: 'assign',
+        title: '分配'
+      }
+    ],
+    count: 4
+  }
+})
+
+const hadRoleList = ref<Array<any>>([])
+const addRoleList = ref<Array<any>>([])
+const delRoleList = ref<Array<any>>([])
 const drawer = ref({
   title: '新增用户',
   placement: 'left',
   visible: false,
   row: {},
-  form: {
-    fields: [
-      {
-        title: '所属机构',
-        name: 'orgId',
-        required: true,
-        control: 'unione-select-box',
-        convert: {
-          types: 'local',
-          url: '/api/system/organ/find',
-          labelField: 'name',
-          search: true
-        }
-      },
-      {
-        title: '用户类型',
-        name: 'userType',
-        control: 'unione-select-box',
-        value: 2,
-        convert: {
-          types: 'dict',
-          dictName: 'USERTYPE'
-        }
-      },
-      {
-        title: '用户帐号',
-        name: 'username',
-        required: true
-      },
-      {
-        title: '用户密码',
-        name: 'pwdText',
-        control: 'unione-pwd-box',
-        required: true,
-        event: {
-          visible: (value: any, ctx: any) => {
-            return !ctx.id
-          }
-        }
-      },
-      {
-        title: '用户姓名',
-        name: 'realName',
-        required: true
-      },
-      {
-        title: '用户性别',
-        name: 'sex',
-        control: 'unione-switch-box',
-        value: 2,
-        convert: {
-          types: 'dict',
-          dictName: 'SEX'
-        }
-      },
-      {
-        title: '出生日期',
-        name: 'birthday',
-        control: 'a-date-picker',
-        props: {
-          placeholder: '',
-          valueFormat: 'YYYY-MM-DD'
-        }
-      },
-      {
-        title: '手机号码',
-        name: 'tel',
-        required: true
-      },
-      {
-        title: '邮箱地址',
-        name: 'email'
-      },
-      {
-        title: 'QQ号码',
-        name: 'qq'
-      },
-      {
-        title: '用户状态',
-        name: 'status',
-        control: 'unione-select-box',
-        value: 1,
-        convert: {
-          types: 'dict',
-          dictName: 'USERSTATUS'
-        }
-      },
-      {
-        title: '备注',
-        control: 'a-textarea',
-        name: 'descs'
-      }
-    ],
-    setting: {
-      showColumn: 1,
-      labelWidth: 5
-    }
-  },
-  tosave: () => {
-    form.value.validate().then((data: any) => {
-      data = {
-        ...drawer.value.row,
-        ...data
-      }
-      page.value
-        .storage()
-        .save({ data })
-        .then(() => {
-          drawer.value.visible = false
-          page.value.reload()
-        })
-    })
-  }
+  tosave: () => {}
 })
 </script>
 
