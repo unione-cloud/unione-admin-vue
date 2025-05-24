7010defa16d5cddb2ab0134a96206dac9dc66ec6
unione-cloud <unione-cloud@163.com>
2025-05-24 08:59:14 +0800

    角色资源分配

--- src/views/system/permis/role/list.vue
+++ src/views/system/permis/role/list.vue
@@ -3,29 +3,25 @@
   <div class="unione-page unione-page-list unione-system-role-permis">
     <unione-page-list ref="page" v-bind="define" @btnClick="btnClick"></unione-page-list>
 
-    <a-drawer
-      :title="drawer.title"
-      :width="550"
-      v-model:visible="drawer.visible"
-      :placement="drawer.placement"
-      class="drawer-form"
-    >
-      <unione-form :form="drawer.form" ref="form"></unione-form>
-
-      <div class="btns">
-        <a-button type="primary" @click="drawer.tosave">保存</a-button>
-        <a-button @click="drawer.visible = false">取消</a-button>
-      </div>
-    </a-drawer>
+    <ResSelect
+      v-model:visible="resSelectVisible"
+      targetType="role"
+      :targetValue="targetObj?.id"
+      position="right"
+      @ok="handelResOk"
+    ></ResSelect>
   </div>
 </template>
 
 <script setup lang="ts">
 import { nextTick, ref } from 'vue'
 import { useRouter, type Router } from 'vue-router'
+import { axios, useDialog } from 'unione-base-vue'
 
+const dialog = useDialog()
 const router: Router = useRouter()
 const page = ref()
+
 const define = ref({
   storage: {
     controller: '/api/system/role'
@@ -94,95 +90,32 @@ const define = ref({
 })
 
 function btnClick({ btn, event, row, keys }: any) {
-  console.log('table btn click', btn, event, row)
-  if (btn.name == 'add') {
-    drawer.value.visible = true
-    drawer.value.title = '新增角色'
-    drawer.value.placement = 'left'
-    drawer.value.row = {}
-    nextTick(() => {
-      form.value.reset()
-    })
-  }
-  if (btn.name == 'edit') {
-    drawer.value.visible = true
-    drawer.value.title = '编辑角色'
-    drawer.value.placement = 'right'
-    drawer.value.row = row
-    nextTick(() => {
-      form.value.setValue(row)
-    })
+  if (btn.name == 'res-assign') {
+    resSelectVisible.value = true
+    targetObj.value = row
   }
   if (btn.name == 'userlist') {
     router.push({ path: '/system/permis/role/users', query: { roleId: row.id } })
   }
 }
-
-const form = ref() //form ref obj
-const drawer = ref({
-  title: '新增角色',
-  placement: 'left',
-  visible: false,
-  row: {},
-  form: {
-    fields: [
-      {
-        title: '角色名称',
-        name: 'name',
-        required: true
-      },
-      {
-        title: '角色编码',
-        name: 'sn',
-        required: true
-      },
-      {
-        title: '角色类型',
-        name: 'types',
-        control: 'unione-select-box',
-        value: 9,
-        convert: {
-          types: 'dict',
-          dictName: 'ROLETYPE'
-        }
-      },
-      {
-        title: '角色状态',
-        name: 'status',
-        control: 'unione-select-box',
-        value: 1,
-        convert: {
-          types: 'dict',
-          dictName: 'USERSTATUS'
-        }
-      },
-      {
-        title: '备注',
-        control: 'a-textarea',
-        name: 'descs'
-      }
-    ],
-    setting: {
-      showColumn: 1,
-      labelWidth: 5
-    }
-  },
-  tosave: () => {
-    form.value.validate().then((data: any) => {
-      data = {
-        ...drawer.value.row,
-        ...data
-      }
-      page.value
-        .storage()
-        .save({ data })
-        .then(() => {
-          drawer.value.visible = false
-          page.value.reload()
-        })
+const targetObj = ref<any>(null)
+const resSelectVisible = ref(false)
+function handelResOk(e: any) {
+  console.log('res select ok', e)
+  const resList = e.list.map((item: any) => {
+    return { ...item, roleId: targetObj.value.id }
+  })
+  axios
+    .admin({
+      url: `/api/system/rolePermis/save`,
+      method: 'post',
+      data: { roleId: targetObj.value.id, addPermis: resList }
     })
-  }
-})
+    .then(() => {
+      dialog.success('资源分配成功')
+      resSelectVisible.value = false
+    })
+}
 </script>
 
 <style scoped lang="less">
