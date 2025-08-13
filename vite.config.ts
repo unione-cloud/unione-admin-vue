d5bdc59f8257d64fc79a4dae038ae55945e67f69
jeking <jeking217@163.com>
2025-08-13 16:45:58 +0800

    修改：portal path

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig(() => {
   const model = process.env.model || 'dev'
   return {
-    base: model === 'dev' ? '/' : '/portal',
+    base: model === 'dev' ? '/' : '/',
     plugins: [vue(), VueDevTools()],
     resolve: {
       alias: {
