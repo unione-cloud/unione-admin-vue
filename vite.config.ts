60c69c6349e9d13c2e2333beb681f1cd1def62af
jeking <jeking217@163.com>
2025-08-13 19:01:24 +0800

    修改dev配置

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,20 @@ export default defineConfig(() => {
           changeOrigin: true // 搭建代理服务器，开启代理
           // secure: true, // 是否代理https接口
           // rewrite: path => path.replace('', '') // 不用路径重写
+        },
+        '/cms-web': {
+          // target: 'http://localhost:8080',
+          target: 'http://8.134.11.253:8090', // 代理的目标地址
+          changeOrigin: true // 搭建代理服务器，开启代理
+          // secure: true, // 是否代理https接口
+          // rewrite: path => path.replace('', '') // 不用路径重写
+        },
+        '/cms': {
+          // target: 'http://localhost:8080',
+          target: 'http://8.134.11.253:8090', // 代理的目标地址
+          changeOrigin: true // 搭建代理服务器，开启代理
+          // secure: true, // 是否代理https接口
+          // rewrite: path => path.replace('', '') // 不用路径重写
         }
       }
     }
