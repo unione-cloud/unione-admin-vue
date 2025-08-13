ed9af9baabf5d0620e386f63695f616770effb33
jeking <jeking217@163.com>
2025-08-13 12:15:16 +0800

    处理：build 问题

--- vite.config.ts
+++ vite.config.ts
@@ -5,31 +5,35 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), VueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true
+export default defineConfig(() => {
+  const model = process.env.model || 'dev'
+  return {
+    base: model === 'dev' ? '/' : '/portal',
+    plugins: [vue(), VueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    }
-  },
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    proxy: {
-      '/portal': {
-        // target: 'http://localhost:8080',
-        target: 'http://8.134.11.253:8090', // 代理的目标地址
-        changeOrigin: true // 搭建代理服务器，开启代理
-        // secure: true, // 是否代理https接口
-        // rewrite: path => path.replace('', '') // 不用路径重写
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true
+        }
+      }
+    },
+    build: {
+      outDir: 'dist'
+    },
+    server: {
+      proxy: {
+        '/portal': {
+          // target: 'http://localhost:8080',
+          target: 'http://8.134.11.253:8090', // 代理的目标地址
+          changeOrigin: true // 搭建代理服务器，开启代理
+          // secure: true, // 是否代理https接口
+          // rewrite: path => path.replace('', '') // 不用路径重写
+        }
       }
     }
   }
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,12 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
+const resolvedViteConfig = viteConfig({
+  mode: 'test',
+  command: 'serve'
+})
 export default mergeConfig(
-  viteConfig,
+  resolvedViteConfig,
   defineConfig({
     test: {
       environment: 'jsdom',
