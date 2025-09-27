b2a1b8fd57a7b2dd0ba0a54b74231b0b0ed5541d
jeking <jeking217@163.com>
2025-09-27 13:10:51 +0800

    ai chat

---
+++ src/views/ai/chat.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="unione-page ai-chat">
+        ai chat
+    </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+    name: 'UnioneAiChat'
+})
+
+
+</script>
+
+<style scoped lang="less">
+.ai-chat {
+    height: 100%;
+}
+</style>
