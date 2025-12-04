aee04e55089c8cf0c6f0049998547204e2b6eb78
jeking <jeking217@163.com>
2025-12-04 16:16:48 +0800

    fix: 更新本地开发环境的代理配置
    - 将/portal代理目标从8080端口改为8090端口
    - 将/flow代理目标从8081端口改为8080端口
    - 移除不再使用的/cms-web和/cms代理配置

--- vite.config.ts
+++ vite.config.ts
@@ -32,29 +32,16 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/portal': {
-          target: 'http://localhost:8080',
+          target: 'http://localhost:8090',
+          // target: 'http://localhost:8080',
           // target: 'https://online.unione.cloud', // 代理的目标地址
           changeOrigin: true // 搭建代理服务器，开启代理
           // secure: true, // 是否代理https接口
           // rewrite: path => path.replace('', '') // 不用路径重写
         },
         '/flow': {
-          target: 'http://localhost:8081',
+          target: 'http://localhost:8080',
           // target: 'https://online.unione.cloud', // 代理的目标地址
-          changeOrigin: true // 搭建代理服务器，开启代理
-          // secure: true, // 是否代理https接口
-          // rewrite: path => path.replace('', '') // 不用路径重写
-        },
-        '/cms-web': {
-          // target: 'http://localhost:8080',
-          target: 'https://online.unione.cloud', // 代理的目标地址
-          changeOrigin: true // 搭建代理服务器，开启代理
-          // secure: true, // 是否代理https接口
-          // rewrite: path => path.replace('', '') // 不用路径重写
-        },
-        '/cms': {
-          // target: 'http://localhost:8080',
-          target: 'https://online.unione.cloud', // 代理的目标地址
           changeOrigin: true // 搭建代理服务器，开启代理
           // secure: true, // 是否代理https接口
           // rewrite: path => path.replace('', '') // 不用路径重写
