0a5674254cdc31849a4454974363c152c0a823e1
jeking <jeking217@163.com>
2025-12-31 19:56:01 +0800

    修改api配置

--- vite.config.ts
+++ vite.config.ts
@@ -32,8 +32,8 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/portal': {
-          // target: 'http://localhost:8090',
-          target: 'http://localhost:8080',
+          target: 'http://localhost:8090',
+          // target: 'http://localhost:8080',
           // target: 'https://online.unione.cloud', // 代理的目标地址
           changeOrigin: true // 搭建代理服务器，开启代理
           // secure: true, // 是否代理https接口
