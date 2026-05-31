673a43afa06d5331fdc70d05b61f89835e4b3bc2
jeking <jeking217@163.com>
2026-05-31 18:40:37 +0800

    完善：系统管理列表

--- src/views/dev/sys/list.vue
+++ src/views/dev/sys/list.vue
@@ -3,17 +3,7 @@
   <div class="unione-page unione-page-list unione-system-sys">
     <unione-page-list ref="page" v-bind="define" @btnClick="btnClick"></unione-page-list>
 
-    <a-drawer :title="drawer.title" :width="750" v-model:visible="drawer.visible" :placement="drawer.placement"
-      class="system-sys-drawer-form" :mask-closable="false">
-      <unione-form class="drawer-form" :form="drawer.form" ref="form"></unione-form>
-
-      <template #footer>
-        <div class="btns">
-          <a-button type="primary" @click="drawer.tosave">保存</a-button>
-          <a-button @click="drawer.visible = false">取消</a-button>
-        </div>
-      </template>
-    </a-drawer>
+    <UnioneSystemDesign ref="designRef"></UnioneSystemDesign>
   </div>
 </template>
 
@@ -21,11 +11,11 @@
 import { nextTick, ref } from 'vue'
 import { useDialog } from 'unione-base-vue'
 import { Convertor } from 'unione-form-vue'
-import { useRouter, type Router } from 'vue-router'
 
 const dialog = useDialog()
 const stsConvert = new Convertor({ types: 'dict', dictName: 'APPSTATUS' })
-const router: Router = useRouter()
+
+const designRef = ref()
 
 const page = ref()
 const define = ref({
@@ -46,8 +36,8 @@ const define = ref({
       name: 'isGlobal',
       convert: {
         types: 'dict',
-        dictName: 'TUREORFALSE'
-      }
+        dictName: 'TRUEORFALSE'
+      },
     },
     {
       title: '类型',
@@ -99,15 +89,11 @@ const define = ref({
   ],
   operation: {
     title: '操作',
-    width: 280,
+    width: 250,
     btns: [
       {
         name: 'view',
         title: '预览',
-      },
-      {
-        name: 'design',
-        title: '设计',
       },
       {
         name: 'status',
@@ -149,22 +135,10 @@ const define = ref({
 async function btnClick({ btn, event, row, keys }: any) {
   console.log('table btn click', btn, event, row)
   if (btn.name == 'add') {
-    drawer.value.visible = true
-    drawer.value.title = '新增系统'
-    drawer.value.placement = 'left'
-    drawer.value.row = {}
-    nextTick(() => {
-      form.value.reset()
-    })
+    designRef.value.open()
   }
   if (btn.name == 'edit') {
-    drawer.value.visible = true
-    drawer.value.title = '编辑系统'
-    drawer.value.placement = 'right'
-    drawer.value.row = row
-    nextTick(() => {
-      form.value.setValue(row)
-    })
+    designRef.value.open(row.id)
   }
   if (btn.name.startsWith('sts-')) {
     const status = btn.name.split('-')[1]
@@ -186,126 +160,6 @@ async function btnClick({ btn, event, row, keys }: any) {
   }
 }
 
-const form = ref() //form ref obj
-const drawer = ref({
-  title: '新增系统',
-  placement: 'left',
-  visible: false,
-  row: {},
-  form: {
-    fields: [
-      {
-        title: '系统名称',
-        name: 'name',
-        required: true
-      },
-      {
-        title: '系统简称',
-        name: 'alias',
-      },
-      {
-        title: '系统类型',
-        name: 'types',
-        control: 'unione-select-box',
-        value: 'pc',
-        convert: {
-          types: 'dict',
-          dictName: 'APPTYPES'
-        },
-      },
-      {
-        title: '系统ctx',
-        name: 'ctx',
-        required: true
-      },
-      {
-        title: '系统logo大',
-        name: 'logoLarge',
-        control: 'unione-upload-box',
-        props: {
-          accept: 'image/*',
-          listType: 'picture-card',
-          limit: 1,
-          valueType: 'string',
-          bucket: 'system'
-        }
-      },
-      {
-        title: '系统logo小',
-        name: 'logoSmall',
-        control: 'unione-upload-box',
-        props: {
-          accept: 'image/*',
-          listType: 'picture-card',
-          limit: 1,
-          valueType: 'string',
-          bucket: 'system'
-        }
-      },
-      {
-        title: '版本号',
-        name: 'versNo',
-        value: '0.0.1',
-        required: true
-      },
-      {
-        title: '版本说明',
-        name: 'versDesc',
-        control: 'unione-rich-text',
-        value: '{}'
-      },
-      {
-        title: '系统配置',
-        name: 'configs',
-        control: 'unione-code-editor',
-        props: {
-          lang: 'json'
-        }
-      },
-      {
-        title: '显示顺序',
-        name: 'ordered',
-        value: 0,
-        control: 'a-input-number',
-        required: true
-      },
-      {
-        title: '应用状态',
-        name: 'status',
-        control: 'unione-select-box',
-        value: 1,
-        convert: {
-          types: 'dict',
-          dictName: 'APPSTATUS'
-        }
-      },
-      {
-        title: '备注',
-        control: 'a-textarea',
-        name: 'descs'
-      }
-    ],
-    setting: {
-      showColumn: 1,
-      labelWidth: 5
-    }
-  },
-  tosave: () => {
-    form.value.validate().then((data: any) => {
-      data = {
-        ...drawer.value.row,
-        ...data
-      }
-      page.value
-        .storage()
-        .save({ data })
-        .then(() => {
-          drawer.value.visible = false
-          page.value.reload()
-        })
-    })
-  }
-})
 </script>
 
 <style scoped lang="less">
