e08c4d5025cd3eb639fefe1cae179910794647b5
jeking <jeking217@163.com>
2026-08-07 18:34:39 +0800

    组件整理

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,3 @@
-import UnioneUserSelect from './user-select/index.vue'
-import UnioneRoleSelect from './role-select/index.vue'
-import UnioneRoleInput from './role-select/role-input.vue'
-import UnioneOrganSelect from './organ-select/index.vue'
-import UnioneGroupSelect from './group-select/index.vue'
-import UnionePostSelect from './post-select/index.vue'
 import UnioneResSelect from './res-select/index.vue'
 import UnioneNoticeView from './notice-icon/noticeView.vue'
 import UnioneTargetList from './target-list/index.vue'
@@ -17,12 +11,6 @@ import JobTaskHandleScript from './job-task/handleScript.vue'
 export default {
   install(app: any, options: any) {
     // 配置此应用
-    app.component('UnioneUserSelect', UnioneUserSelect)
-    app.component('UnioneRoleSelect', UnioneRoleSelect)
-    app.component('UnioneRoleInput', UnioneRoleInput)
-    app.component('UnioneOrganSelect', UnioneOrganSelect)
-    app.component('UnioneGroupSelect', UnioneGroupSelect)
-    app.component('UnionePostSelect', UnionePostSelect)
     app.component('UnioneResSelect', UnioneResSelect)
     app.component('UnioneFormSelect', UnioneFormSelect)
     app.component('UnioneTargetList', UnioneTargetList)
